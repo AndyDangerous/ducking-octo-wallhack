c3989793eee987f9613595f67b33d9ba79a6bb7b
Andy Mention <amention@gmail.com>
2015-05-13 23:41:59 -0600

    level 3

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var App = Ember.Application.create({
 App.Router.map(function() {
   this.route('about', { path: '/aboutus' } );
   this.resource('products');
+  this.resource('product', { path: 'products/:title' } );
 });
 
 App.IndexController = Ember.Controller.extend({
@@ -14,3 +15,33 @@ App.IndexController = Ember.Controller.extend({
     return(new Date()).toDateString()
   }.property()
 });
+
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+  return App.PRODUCTS;
+  }
+});
+
+App.ProductRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.PRODUCTS.findBy('title'. params.title);
+  }
+});
+
+App.PRODUCTS = [
+  {
+    title: 'Flint',
+    description: 'Flint is...',
+    isOnSale: true,
+    image: 'flint.png'
+  },
+  {
+    title: 'Tinder',
+    description: 'Tinder is...',
+    isOnSale: false,
+    image: 'tinder.png'
+  }
+];
+
+
