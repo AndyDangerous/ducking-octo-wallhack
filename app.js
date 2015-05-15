3183bde0e9b34c347b3bae38757f256c2a6c9e62
Andy Mention <amention@gmail.com>
2015-05-15 11:22:51 -0600

    level 4

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
   this.route('about', { path: '/aboutus' } );
-  this.resource('products');
-  this.resource('product', { path: 'products/:title' } );
+  this.resource('products', function() {
+    this.resource('product', { path: '/:product_id' } );
+  });
 });
 
 App.IndexController = Ember.Controller.extend({
@@ -19,29 +20,58 @@ App.IndexController = Ember.Controller.extend({
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-  return App.PRODUCTS;
+    return this.store.findAll('product');
   }
 });
 
 App.ProductRoute = Ember.Route.extend({
   model: function(params) {
-    return App.PRODUCTS.findBy('title'. params.title);
+    return this.store.find('product'. params.product_id);
   }
 });
 
-App.PRODUCTS = [
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string'),
+  reviews: DS.hasMany('review', {async: true})
+});
+
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date'),
+  products: DS.belongsTo('product')
+});
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Product.FIXTURES = [
   {
+    id: 1,
     title: 'Flint',
     description: 'Flint is...',
     isOnSale: true,
-    image: 'flint.png'
+    image: 'flint.png',
+    reviews: [100,101]
   },
   {
+    id: 2,
     title: 'Tinder',
     description: 'Tinder is...',
     isOnSale: false,
-    image: 'tinder.png'
+    image: 'tinder.png',
+    reviews: [100,101]
   }
 ];
 
-
+App.Review.FIXTURES = [
+  { id: 100,
+    product: 1,
+    text: 'A thing'
+  },
+  { id: 101,
+    product: 1,
+    text: 'Another thing'
+  }];
